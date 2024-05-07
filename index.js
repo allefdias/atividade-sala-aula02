cb41252e897199eb47288057b88c7e92bac64541
allefdias <147508740+allefdias@users.noreply.github.com>
2024-05-07 10:04:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let nota1= parseFloat(prompt(`Insira a primeira nota: `))
+let nota2= parseFloat(prompt(`Insira a segunda nota: `))
+let nota3= parseFloat(prompt(`Insira a terceira nota: `))
+let media = (nota1 + nota2 + nota3) / 3
+
+
+let nota1= parseFloat(prompt(`Insira a primeira nota: `))
+let nota2= parseFloat(prompt(`Insira a segunda nota: `))
+let nota3= parseFloat(prompt(`Insira a terceira nota: `))
+let media = (nota1 + nota2 + nota3) / 3
+console.log(`Sua media é ${media}`)
